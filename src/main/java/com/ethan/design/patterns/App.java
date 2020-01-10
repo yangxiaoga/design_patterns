ad1d04cc008d42761494531cabb447e6f665800d
yangxiaoga <18812668537@163.com>
2020-01-10 20:25:29 +0800

    init design patterns project

---
+++ src/main/java/com/ethan/design/patterns/App.java
@@ -0,0 +1,6 @@
+package com.ethan.design.patterns;
+
+public class App {
+    public static void main( String[] args ){
+    }
+}
---
+++ src/main/java/com/ethan/design/patterns/creater/singleton/Singleton.java
@@ -0,0 +1,14 @@
+package com.ethan.design.patterns.creater.singleton;
+
+/**
+ * @author Administrator
+ * 1、核心作用
+ * 保证一个类只有一个实例，并且提供一个访问该实例的全局访问点
+ * -------------------------------------------
+ * 2、什么用的单例模式?
+ * 应用程序的日志,数据库连接池,Spring中bean默认单例
+ * 读取配置文件的类
+ */
+public class Singleton {
+
+}
